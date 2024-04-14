e2705763369482ab22f233bf73287882cfa99be8
Vansh <vanshwaldeo360@gmail.com>
2024-04-14 18:04:27 +0530

    Connected DB and managed routes

---
+++ connect.js
@@ -0,0 +1,18 @@
+const { default: mongoose } = require('mongoose');
+const mogoose = require('mongoose');
+
+const connectDb = () =>{
+    mongoose.connect(process.env.DATABASE_URL , {
+        useNewUrlParser : true,
+        useUnifiedTopology : true,
+    })
+    .then(()=>console.log("Db Connected"))
+    .catch((error)=>{
+
+        console.log("Recieved errord");
+        console.error(error.message);
+        process.exit(1);
+    })
+}
+
+module.exports = connectDb;
--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require('express');
 const app = express();
 const urlRoute = require("./routes/url");
 
-const PORT = 3000;
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
 
 app.use("/url" , urlRoute);
 
-app.get('/' , (req , res) =>{
-    res.send("Hello My name is Vansh");
-})
+// app.get('/' , (req , res) =>{
+//     res.send("Hello My name is Vansh");
+// })
 
 app.listen(PORT , ()=>{
     console.log("The Server is active and is listninig you ");
 })
+
+//db connections 
+const connectDb = require('./connect');
+connectDb();
